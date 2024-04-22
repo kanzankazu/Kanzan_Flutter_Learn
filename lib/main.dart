c70f83ff92c5ca56fa8f715b5483e2b804be53d3
Faisal Bahri <kanzankazu46@gmail.com>
2024-04-23 00:26:36 +0700

    add Class StateFul  hello_world_stateful.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:belajar_1/row_widget.dart';
+import 'package:belajar_1/hello_world_stateful.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -31,48 +31,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  /*@override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }*/
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Belajar Flutter',
-      home: RowWidget(), // tinggal ubah manggilnya
+      home: HelloWorldStateFul(
+        message: "Ini Message",
+      ), // tinggal ubah manggilnya
     );
   }
 }
