cfa9b55b452206476859f3350cc596b9bcae6853
Faisal Bahri <kanzankazu46@gmail.com>
2024-05-07 22:55:47 +0700

    test rebase

--- lib/widgetexample/maps.dart
+++ lib/widgetexample/maps.dart
@@ -18,6 +18,8 @@ class MapSampleState extends State<MapSample> {
     zoom: 14.4746,
   );
 
+  //
+
   static const CameraPosition _kLake = CameraPosition(bearing: 192.8334901395799, target: LatLng(37.43296265331129, -122.08832357078792), tilt: 59.440717697143555, zoom: 19.151926040649414);
 
   @override
